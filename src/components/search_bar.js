01098a28035a40f87ad2c56178b516dd4a28c194
andy <andy@winarcooo.local>
2018-01-01 23:39:02 +0700

    initial commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,30 @@
+/**
+ * Created by andy on 29/11/17.
+ */
+
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div className="search-bar">
+                <input
+                    value={this.state.term}
+                    onChange={ event => this.onInputChange(event.target.value) }/>
+            </div>
+        );
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
+}
+
+export default SearchBar;
